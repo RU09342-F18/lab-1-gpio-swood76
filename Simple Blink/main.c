d27719ac6606c7e2f8c266992a404e4c26721d42
swood76 <35699946+swood76@users.noreply.github.com>
2018-09-21 19:31:34 -0400

    Add files via upload

---
+++ Simple Blink/main.c
@@ -0,0 +1,23 @@
+#include <msp430.h>
+#define GREEN_LED BIT6 // Port 1.6
+int main(void)
+{
+  WDTCTL = WDTPW + WDTHOLD;                 // Stop watchdog timer
+  P1DIR |= 0x01;                            // Set P1.0 to output direction
+
+  for (;;)
+  {
+    volatile unsigned int i;
+
+    P1OUT ^= 0x01;                          // Toggle P1.0 using exclusive-OR
+
+    i = 50000;                              // Delay
+    do (i--);
+    while (i != 0);
+    {
+
+    }
+
+  }
+}
+
