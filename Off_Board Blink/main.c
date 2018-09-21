f8ef8546c7157a09d957592a119c0cf24c6c36b9
swood76 <35699946+swood76@users.noreply.github.com>
2018-09-21 18:36:06 -0400

    Update main.c

--- Off_Board Blink/main.c
+++ Off_Board Blink/main.c
@@ -1,12 +1,55 @@
-#include <msp430.h> 
+#include <msp430g2553.h>
 
 
-/**
- * main.c
- */
-int main(void)
+
+// Blink time constants
+
+#define CTR1 3000
+
+#define CTR2 3000
+
+// led bit assignment
+
+#define LED1 BIT0
+
+#define LED2 BIT6
+
+
+
+void main(void)
+
 {
-	WDTCTL = WDTPW | WDTHOLD;	// stop watchdog timer
-	
-	return 0;
+
+int i,j;
+
+WDTCTL = WDTPW + WDTHOLD;
+
+P1DIR |= (LED1|LED2);
+
+i=j=0;
+
+for(;;)
+
+{ // increment both counter
+
+  i++;
+
+  j++;
+
+// check first counter, if end value reset and toggle led
+
+  if (i>CTR1)
+
+      { i=0; P1OUT^=LED1;}
+
+// check 2nd counter, if end value reset and toggle led
+  if (j>CTR2)
+
+     { j=0; P1OUT^=LED2;}
+
+} // forever
+
+// End main
+
 }
+
